46b2e50651143150ad5f20620f2a7c98fc4e3925
KylerORourke <kyler.j.orourke@gmail.com>
2024-11-22 17:35:52 -0500

    testing folder

---
+++ rustbasics/hello/target/debug/deps/hello-84e3f7c654170da7.d
@@ -0,0 +1,5 @@
+/Users/kyler.orourke/Dev/rustbasics/hello/target/debug/deps/hello-84e3f7c654170da7: src/main.rs
+
+/Users/kyler.orourke/Dev/rustbasics/hello/target/debug/deps/hello-84e3f7c654170da7.d: src/main.rs
+
+src/main.rs:
---
+++ rustbasics/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/kyler.orourke/Dev/rustbasics/hello/target/debug/hello: /Users/kyler.orourke/Dev/rustbasics/hello/src/main.rs
