ef20cd8df9fb0270de78d0f90d3fafa17eb70888
KylerORourke <kyler.j.orourke@gmail.com>
2024-11-24 23:41:28 -0600

    added cli test

--- rustbasics/hello/tests/cli.rs
+++ rustbasics/hello/tests/cli.rs
@@ -1,5 +1,14 @@
-#[test]
+use std::process::Command; #[test]
 
-fn works () { 
-    assert!(true); 
+fn runs () { 
+
+    let mut cmd = Command::new("hello"); 
+
+
+    let res = cmd.output(); 
+
+    assert!(res.is_ok()); 
+
 }
+
+
