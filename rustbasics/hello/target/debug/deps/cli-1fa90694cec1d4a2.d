e59b224033276e863f96bbf876de5e2672ca5c97
KylerORourke <kyler.j.orourke@gmail.com>
2024-11-24 23:40:20 -0600

    added cli test

---
+++ rustbasics/hello/target/debug/deps/cli-1fa90694cec1d4a2.d
@@ -0,0 +1,5 @@
+/Users/kyler.orourke/Dev/rustbasics/hello/target/debug/deps/cli-1fa90694cec1d4a2: tests/cli.rs
+
+/Users/kyler.orourke/Dev/rustbasics/hello/target/debug/deps/cli-1fa90694cec1d4a2.d: tests/cli.rs
+
+tests/cli.rs:
---
+++ rustbasics/hello/target/debug/deps/hello-dc313c540fccf7be.d
@@ -0,0 +1,5 @@
+/Users/kyler.orourke/Dev/rustbasics/hello/target/debug/deps/hello-dc313c540fccf7be: src/main.rs
+
+/Users/kyler.orourke/Dev/rustbasics/hello/target/debug/deps/hello-dc313c540fccf7be.d: src/main.rs
+
+src/main.rs:
